1c89d9642a24171a57ca0de097e6fe20583e2859
hoffman <hoffmanshf@outlook.com>
2020-08-14 14:41:07 -0400

    connect the application to MongoDB

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import "express-async-errors";
 import { json } from "body-parser";
+import mongoose from "mongoose";
+
 import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
 import { signoutRouter } from "./routes/signout";
@@ -21,6 +23,21 @@ app.all("*", async () => {
 });
 app.use(errorHandler);
 
+const start = async () => {
+  try {
+    // a database named auth will be automatically created by mongodb
+    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+    console.log("connected to Mongo");
+  } catch (err) {
+    console.log(err);
+  }
+};
 app.listen(3000, () => {
   console.log("Listening on port 3000");
 });
+
+start();
