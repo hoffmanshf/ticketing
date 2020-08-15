23069a0a89fdd61da39aab16db9b31588e780a09
hoffman <hoffmanshf@outlook.com>
2020-08-14 22:25:06 -0400

    finish test suite for API requests

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,10 +1,22 @@
 import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose from "mongoose";
+import request from "supertest";
+import { app } from "../app";
+
+// augment the type definition of NodeJS Global field
+declare global {
+  namespace NodeJS {
+    interface Global {
+      // returns a promise that will be resolve with array of string
+      signin(): Promise<string[]>
+    }
+  }
+}
 
 let mongo: any;
 // before all the tests run
 beforeAll(async () => {
-  process.env.JWT_KEY = "asdfgh"
+  process.env.JWT_KEY = "asdfgh";
   mongo = new MongoMemoryServer();
   const mongoUri = await mongo.getUri();
 
@@ -29,3 +41,21 @@ afterAll(async () => {
   await mongo.stop;
   await mongoose.connection.close();
 });
+
+// create a global function to avoid import from setup file in every test suite
+global.signin = async () => {
+  const email = "test@test.com";
+  const password = "password";
+
+  const response = await request(app)
+    .post("/api/users/signup")
+    .send({
+      email,
+      password,
+    })
+    .expect(201);
+
+  const cookie = response.get("Set-Cookie");
+
+  return cookie;
+};
