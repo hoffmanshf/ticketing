ddbeefa46ea5dc4a8cbaa256e0ed4e1096f88ebc
hoffman <hoffmanshf@outlook.com>
2020-08-24 12:44:40 -0400

    implemented expiration complete event listener and test suite in order service

---
+++ orders/src/events/listeners/expiration-complete-listener.ts
@@ -0,0 +1,45 @@
+import {
+  Listener,
+  Subjects,
+  ExpirationCompleteEvent,
+  OrderStatus,
+} from "@hoffmanshf-ticketing/common";
+import { Message } from "node-nats-streaming";
+import { queueGroupName } from "./queue-group-name";
+import { Order } from "../../models/order";
+import { OrderCancelledPublisher } from "../publishers/order-cancelled-publisher";
+
+export class ExpirationCompleteListener extends Listener<
+  ExpirationCompleteEvent
+> {
+  queueGroupName = queueGroupName;
+  subject: Subjects.ExpirationComplete = Subjects.ExpirationComplete;
+
+  async onMessage(data: ExpirationCompleteEvent["data"], msg: Message) {
+    const order = await Order.findById(data.orderId).populate("ticket");
+
+    if (!order) {
+      throw new Error("Order not found");
+    }
+    if (order.status === OrderStatus.Complete) {
+      return msg.ack();
+    }
+
+    // no need to set ticket to null, since OrderStatus.Cancelled is not considered  as reserved
+    // even if order is cancelled, user can still see what ticket is associated with this order
+    order.set({
+      status: OrderStatus.Cancelled,
+    });
+    await order.save();
+
+    await new OrderCancelledPublisher(this.client).publish({
+      id: order.id,
+      version: order.version,
+      ticket: {
+        id: order.ticket.id,
+      },
+    });
+
+    msg.ack();
+  }
+}
