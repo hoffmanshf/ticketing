53db7f90dd96be64f5611ef31cb5c406ad4596ac
hoffman <hoffmanshf@outlook.com>
2020-08-25 11:23:58 -0400

    test

--- expiration/src/events/listeners/order-created-listener.ts
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -13,7 +13,7 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
 
   async onMessage(data: OrderCreatedEvent["data"], msg: Message) {
     const delay = new Date(data.expiresAt).getTime() - new Date().getTime();
-    console.log("Waiting this many milliseconds to process the job:", delay);
+    console.log("Waiting this many milliseconds to process the job: ", delay);
 
     await expirationQueue.add(
       {
